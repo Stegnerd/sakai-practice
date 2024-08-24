cf30b02e8e6443853e08d9b33014be3f172ec1d3
Dalton Stegner <dmstegner@gmail.com>
2024-08-24 11:18:38 -0400

    app configuration

---
+++ src/components/FloatingConfiguration.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+import AppConfigurator from '@/layout/AppConfigurator.vue';
+
+const { toggleDarkMode, isDarkTheme } = useLayout();
+</script>
+
+<template>
+    <div class="fixed flex gap-4 top-8 right-8">
+        <Button :icon="{ 'pi pi-moon': isDarkTheme, 'pi pi-sun': !isDarkTheme }" rounded severity="secondary" type="button" @click="toggleDarkMode"></Button>
+        <div class="relative">
+            <Button
+                v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
+                icon="pi pi-palette"
+                rounded
+                type="button"
+            />
+            <AppConfigurator />
+        </div>
+    </div>
+</template>
